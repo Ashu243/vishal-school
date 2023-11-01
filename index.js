3b3c3eb715fa028659d82cdcc8330edd73ce5caf
Ashu243 <ashuv6309@gmail.com>
2023-11-01 15:49:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define routes for your HTML files
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+app.get('/class11', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'class11.html'));
+});
+
+app.get('/class12', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'class12.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
